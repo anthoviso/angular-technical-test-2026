4d72e9072e12ed8029f952e64950deb642521957
VISOCCHI <anthonyvisocchi@gmail.com>
2026-02-27 15:32:43 +0100

    feat(sanitize): allow description to be used as html but sanitize data

--- app/src/app/features/categories/components/category/category.component.ts
+++ app/src/app/features/categories/components/category/category.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, input, SecurityContext } from '@angular/core';
 import { TagComponent } from '@shared/components/tag/tag.component';
 import { Category } from '../../categories.models';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-category',
@@ -10,7 +11,17 @@ import { Category } from '../../categories.models';
   imports: [TagComponent],
 })
 export class CategoryComponent {
+  private readonly domSanitizer = inject(DomSanitizer);
+
   readonly category = input.required<Category>();
   readonly showGroupTag = input<boolean>(true);
   readonly isActive = input<boolean>(false);
+
+  protected readonly description = computed<string>(() => {
+    if (!this.category().description) {
+      return 'Aucune description';
+    }
+
+    return this.domSanitizer.sanitize(SecurityContext.HTML, this.category().description) ?? 'Aucune description';
+  });
 }
